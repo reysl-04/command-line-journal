7028d9869d4ed0700ff7b29a59a7055a6c6929d6
reysl-04 <alex.reyesl0410@gmail.com>
2026-04-23 15:59:47 -0400

    feat: add entry

---
+++ commands/add.js
@@ -0,0 +1,19 @@
+import { readEntry, writeEntry } from "storage.js";
+
+function add(text) {
+    if (!text || text.trim() === '') {
+        console.error("Error: entry text can't be empty")
+        process.exit(1)
+    }
+
+    const entries = readEntry();
+    const entry = {
+        id: Date.name(),
+        date: new Date().toISOString(),
+        text: text.trim()
+    };
+
+    entries.push(entry);
+    writeEntry(entries);
+    console.log(`Added entry [${entry.id}]`)
+}
